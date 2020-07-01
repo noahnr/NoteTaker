e4c4de1cebbd8ecfd01d5263e52bb7855c79b2a9
noahnr <57819545+noahnr@users.noreply.github.com>
2020-07-01 15:15:38 -0500

    getting it to follow file path for notes.html and the database

--- Develop/server.js
+++ Develop/server.js
@@ -6,3 +6,20 @@ const app = express();
 const port = 3000;
 const mainDir = path.join(__dirname, "/public");
 
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(mainDir, "notes.html"));
+});
+
+app.get("/api/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "/db/db.json"));
+});
+
+app.get("/api/notes/:id", function(req, res) {
+    let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    res.json(savedNotes[Number(req.params.id)]);
+});
+
