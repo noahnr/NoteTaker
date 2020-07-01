db2956ee6dab73d3f4b8635aa75ceb4687f04090
noahnr <57819545+noahnr@users.noreply.github.com>
2020-07-01 15:17:37 -0500

    Finally got it to listen to port

--- Develop/server.js
+++ Develop/server.js
@@ -39,3 +39,24 @@ app.post("/api/notes", function(req, res) {
     res.json(savedNotes);
 })
 
+app.delete("/api/notes/:id", function(req, res) {
+    let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    let noteID = req.params.id;
+    let newID = 0;
+    console.log(`Deleting note with ID ${noteID}`);
+    savedNotes = savedNotes.filter(currNote => {
+        return currNote.id != noteID;
+    })
+    
+    for (currNote of savedNotes) {
+        currNote.id = newID.toString();
+        newID++;
+    }
+
+    fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
+    res.json(savedNotes);
+})
+
+app.listen(port, function() {
+    console.log(`Now listening to port ${port}. Enjoy your stay!`);
+})
